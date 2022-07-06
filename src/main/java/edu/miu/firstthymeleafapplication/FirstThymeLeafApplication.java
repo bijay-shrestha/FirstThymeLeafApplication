63e5fd866c9ece510c5f51db64e1414cabd93bb5
bijay-shrestha <8ijay.shrestha@gmail.com>
2022-07-06 11:39:38 -0500

    Spring Boot Application Initialized with Database Connection

---
+++ src/main/java/edu/miu/firstthymeleafapplication/FirstThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.firstthymeleafapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstThymeLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstThymeLeafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/firstthymeleafapplication/FirstThymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.firstthymeleafapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstThymeLeafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
